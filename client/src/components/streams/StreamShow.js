2465ad0d58c1bfa86dbbcf79b0e818dae7d85740
chanh <chungminhchanh1999@gmail.com>
2020-06-25 01:04:53 +0700

    implementing flv

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,25 +1,55 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
+import flv from "flv.js";
 
-class StreamShow extends React.Component 
-{
-  componentDidMount() 
-  {
-    this.props.fetchStream(this.props.match.params.id);
+class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
   }
 
-  render() 
-  {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
+
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
+  render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
 
     return (
-        <div>
-            <h1>{this.props.stream.title}</h1>
-            <h5>{this.props.stream.description}</h5>
-        </div>
+      <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls={true} />
+        <h1>{this.props.stream.title}</h1>
+        <h5>{this.props.stream.description}</h5>
+      </div>
     );
   }
 }
